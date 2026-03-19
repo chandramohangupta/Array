c33f62c09b730fbc3a8370bc612d83a434752e71
Chandramohan Gupta <jimohan25261@gmail.com>
2026-03-19 23:20:37 +0530

    Delete ALL Understanding directory

--- ALL Understanding/webapplication/script.js
+++
@@ -1,5 +0,0 @@
-function showMessage() {
-    var name = document.getElementById("name").value;
-    document.getElementById("output").innerHTML =
-        "Hello " + name + "! Welcome to my web app.";
-}
