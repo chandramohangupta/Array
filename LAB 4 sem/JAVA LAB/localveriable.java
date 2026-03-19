113c9c2840c125b029455f5b554151dee930b4e3
Chandramohan Gupta <jimohan25261@gmail.com>
2026-03-19 23:30:29 +0530

    Delete LAB 4 sem directory

--- LAB 4 sem/JAVA LAB/localveriable.java
+++
@@ -1,8 +0,0 @@
-public class localveriable {
-    static int add(int a, int b){
-        return a+b;
-    }
- public static void main(String[] args){
-    System.out.println("addition of two No : "+add(3,6));
- }
-} 
